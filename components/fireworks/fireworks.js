d84ac091245138df7b3c7ab2abfedc104918fcaf
Gill Abada <gill@callifi.com>
2019-02-06 10:34:08 -0500

    remove some debugging.

--- components/fireworks/fireworks.js
+++ components/fireworks/fireworks.js
@@ -1,7 +1,6 @@
 let audio = new Audio('sounds/Fireworks.mp3');
 const fireworks = document.querySelector('.bottom-img');
 const mq = window.matchMedia( "(max-width: 800px)" );
-console.log(mq);
 if (mq.matches) {
     fireworks.addEventListener('click', e => {
         let playPromise = audio.play();
